9f07708bd636d7e967f2f75c65b55ca785682529
David Gilson <davgilson@live.fr>
2018-04-07 01:28:00 +0200

    fix url translate on server side

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { UniversalLocalizeModuleMapNgFactoryLoader } from './universal-localize-module-map-ngfactory-loader';
 
 @NgModule({
   imports: [
@@ -13,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   providers: [
     // Add universal-only providers here
+    {
+      provide: NgModuleFactoryLoader,
+      useClass: UniversalLocalizeModuleMapNgFactoryLoader
+    }
   ],
   bootstrap: [ AppComponent ],
 })
 export class AppServerModule {}
+
