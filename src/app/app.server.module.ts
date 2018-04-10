1ce85da008f7b700196b6a234e01795b16997808
David Gilson <davgilson@live.fr>
2018-04-11 00:31:29 +0200

    change import order

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, NgModuleFactoryLoader } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { LazyUniversalModuleLoaderProvider } from 'localize-router-lazy-universal-module-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { LazyUniversalModuleLoaderProvider } from 'localize-router-lazy-universal-module-loader';
 
 @NgModule({
   imports: [
