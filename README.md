b7978de1d8152241fd72a63841ad82d145693b11
David Gilson <davgilson@live.fr>
2018-04-10 21:43:51 +0200

    fix url not changed

---
+++ README.md
@@ -0,0 +1,15 @@
+# angular-universal-localize-router
+
+Angular (5) + universal + ngx-translate + localize-router + lazyloading
+
+## Development server
+
+Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `npm run build:ssr` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Universal server
+
+Run `npm run serve:ssr` for a universal server. Navigate to `http://localhost:4000/`.
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import {LocalizeRouterService} from 'localize-router';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -10,7 +11,7 @@ import { HeroService } from '../hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private localizeService: LocalizeRouterService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -20,4 +21,9 @@ export class DashboardComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
+
+  switchLanguage(lang) {
+    this.localizeService.changeLanguage(lang);
+  }
+
 }
